e641b8531aa403ffaea36176825e3de9da443687
Neville Mpiana <111021202+NevsCode@users.noreply.github.com>
2022-09-06 20:29:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProgPoE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IncomeExpense incomeExpense = new IncomeExpense();
+            incomeExpense.expenseAmounts();
+            Console.ReadKey();
+        }
+    }
+}
